4b3936e3f28cebcacf85057d6e6907cea1abb9c3
KMSDINESH <dineshkumar54466@gmail.com>
2025-07-22 20:15:16 +0100

    Added small updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mocktails_demo_website/',
+  base: '/Mocktails_demo_website',
   plugins: [react(), tailwindcss()],
 })
